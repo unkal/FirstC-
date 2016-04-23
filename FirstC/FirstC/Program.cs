ba8fd570ffe4874da1e2daadf6b7c3fb0d11061f
unkal <unkal@yandex.ru>
2016-04-23 11:06:22 +0300

    Horizontal

--- FirstC/FirstC/Program.cs
+++ FirstC/FirstC/Program.cs
@@ -11,10 +11,12 @@ namespace FirstC
         static void Main(string[] args)
         {
             Point p1 = new Point(1,3,'*');
-            p1.Draw();
-
             Point p2 = new Point(4,5,'#');
-            p2.Draw();
+
+            p1.Draw();p2.Draw();
+
+            HorizontalLine line = new HorizontalLine(5,10,8,'-');
+            line.Draw();
 
             Console.ReadLine();
         }
